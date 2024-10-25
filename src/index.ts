9fdc8b14987d375569c84dac70baee698d92eebf
RedlicaDev <marcin.redlica@gmail.com>
2024-10-25 23:46:15 +0200

    fixes

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,33 @@ import { scrape } from "./scrape";
 const app = new Hono();
 
 app.get(
-  "/",
-  zValidator(
-    "query",
-    z.object({
-      url: z.string().url(),
-      markdown: z.union([z.literal("true"), z.literal("false")]).optional(),
-    })
-  ),
-  async (c) => {
-    const url = c.req.query("url")!;
-    const markdownParam = c.req.query("markdown") || "true";
-    const markdown = markdownParam === "true" || markdownParam === "1";
-    try {
-      console.log("scraping", url, markdown);
-      const page = await scrape({ url, markdown });
-      return c.json({ page });
-    } catch (e) {
-      if (e instanceof Error) {
-        return c.json({ page: null, error: e.message });
-      } else {
-        return c.json({ page: null, error: "An unknown error occurred" });
-      }
+    "/",
+    zValidator(
+        "query",
+        z.object({
+            url: z.string().url(),
+        })
+    ),
+    async (c) => {
+        const query = c.req.valid("query");
+
+        const url = query["url"];
+
+        try {
+            const page = await scrape({ url });
+
+            return c.json({ page });
+        } catch (e) {
+            if (e instanceof Error) {
+                return c.json({ page: null, error: e.message });
+            } else {
+                return c.json({
+                    page: null,
+                    error: "An unknown error occurred",
+                });
+            }
+        }
     }
-  }
 );
 
 export default app;
--- src/scrape.ts
+++ src/scrape.ts
@@ -2,55 +2,46 @@ import { Readability } from "@mozilla/readability";
 import { parseHTML } from "linkedom";
 import TurndownService from "./turndown";
 
-export const scrape = async ({
-  url,
-  markdown,
-}: {
-  url: string;
-  markdown: boolean;
-}) => {
-  const response = await fetch(url, {
-    headers: {
-      "User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)",
-    },
-  });
-  const html = await response.text();
-  console.log("html", html);
-  const article = extract(html);
+export const scrape = async ({ url }: { url: string }) => {
+    const response = await fetch(url, {
+        headers: {
+            "User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)",
+        },
+    });
+    const html = await response.text();
 
-  if (article == null) {
-    return null;
-  }
+    const article = extract(html);
 
-  if (markdown) {
-    const textContent = convertToMarkdown(article.content);
-    return { ...article, textContent };
-  } else {
+    if (article == null) {
+        return null;
+    }
+
     const content = cleanString(article.content);
     const textContent = cleanString(article.textContent);
+    const markdownContent = convertToMarkdown(content);
 
-    return { ...article, content, textContent };
-  }
+    return { ...article, content, textContent, markdownContent };
 };
 
 const extract = (html: string) => {
-  var doc = parseHTML(html);
-  let reader = new Readability(doc.window.document);
-  return reader.parse();
+    var doc = parseHTML(html);
+    let reader = new Readability(doc.window.document);
+
+    return reader.parse();
 };
 
 const convertToMarkdown = (html: string) => {
-  const turndown = new TurndownService();
-  const doc = parseHTML(html);
-  return turndown.turndown(doc.window.document);
+    const turndown = new TurndownService();
+    const doc = parseHTML(html);
+
+    return turndown.turndown(doc.window.document);
 };
 
-//
 const cleanString = (str: string) =>
-  str
-    // Replace various whitespace and zero-width characters with a single space
-    .replace(/[\s\t\u200B-\u200D\uFEFF]+/g, " ")
-    // Remove leading whitespace from each line in the string
-    .replace(/^\s+/gm, "")
-    // Collapse multiple newline characters into a single newline
-    .replace(/\n+/g, "\n");
+    str
+        // Replace various whitespace and zero-width characters with a single space
+        .replace(/[\s\t\u200B-\u200D\uFEFF]+/g, " ")
+        // Remove leading whitespace from each line in the string
+        .replace(/^\s+/gm, "")
+        // Collapse multiple newline characters into a single newline
+        .replace(/\n+/g, "\n");
